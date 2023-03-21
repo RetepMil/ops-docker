5a54f4bb1cc82d75f281553741d46cfdbed281d3
root <peterlim@testtest.asia-northeast3-a.c.empyrean-yeti-380600.internal>
2023-03-21 00:10:23 +0000

    initial commit

---
+++ src/main/java/com/example/ops_docker_proj/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.ops_docker_proj;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "DEV";
+	}
+
+}
---
+++ src/main/java/com/example/ops_docker_proj/OpsDockerProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.ops_docker_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsDockerProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpsDockerProjApplication.class, args);
+	}
+
+}
