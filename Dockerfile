938a2cb94ea03c30615dbaadb8cd66b738468f06
root <root@gitlab.com>
2023-03-23 01:29:14 +0000

    fix var

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,9 @@
 FROM openjdk:17
 
 ARG JAR_FILE=target/*.jar
-ARG DEPLOY_PORT
-ARG MESSAGE
 
 COPY ${JAR_FILE} app.jar
 
-# ENTRYPOINT ["java", "-jar", "app.jar"]
-ENTRYPOINT echo ${DEPLOY_PORT} && echo ${MESSAGE}
-# ENTRYPOINT java -jar app.jar -Dspring-boot.run.arguments="--server.port=${DEPLOY_PORT} --message='${MESSAGE}'"
+ENTRYPOINT java -jar -Dspring.profiles.active=$MESSAGE app.jar
 
 EXPOSE ${DEPLOY_PORT}
--- src/main/java/com/example/ops_docker_proj/HelloController.java
+++ src/main/java/com/example/ops_docker_proj/HelloController.java
@@ -1,21 +1,16 @@
 package com.example.ops_docker_proj;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	@Value("${server.port}")
-    private Integer serverPort;
-
-	@Value("${message}")
-    private String message;
-
+	
 	@GetMapping("/hello")
 	public ResponseEntity<String> hello() {
-		return ResponseEntity.ok().body(message + "listening on " + serverPort.toString());
+		String message = System.getProperty("spring.profiles.active");
+		return ResponseEntity.ok(message);
 	}
 
 }
